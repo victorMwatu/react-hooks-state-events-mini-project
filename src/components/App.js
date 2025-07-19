fb015352dce2c8450bbd6567a916ab1e4c2e89f3
victorMwatu <victormwatu@gmail.com>
2025-07-19 13:40:38 +0300

    passes two out of four tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,18 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskList, setTaskList] = useState(TASKS);
+
+  function handleDelete(text) {
+  setTaskList( taskList.filter(t => t.text != text))
+ }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={taskList} onDelete={handleDelete} />
     </div>
   );
 }
